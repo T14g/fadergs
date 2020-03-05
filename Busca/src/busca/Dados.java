178ed85a33febee4b0efcd0f3093509747a3cf0f
TIAGO OLIVEIRA SANTOS <ALU201810206@esade.local>
2020-03-04 21:18:52 -0300

    Corrigir

--- Busca/src/busca/Dados.java
+++ Busca/src/busca/Dados.java
@@ -8,36 +8,43 @@ public class Dados {
     
     //Método sequencial
     public int sequencial(int val) {
-
+        
+        int iteracoes = 1;
+        
         for(int pos = 0; pos < vetor.length; pos++){
             if(val == vetor[pos]){
+                System.out.println("Iteracoes: " + iteracoes);
                 return pos;
             }
+            
+            iteracoes++;
         }
-        
-        return -1;
+        return -1;//not found
     }
     
 //    Método binário 
     public int binario(int val){
         
         int inicio = 0, meio, fim = vetor.length -1;
+        int iteracoes = 1;
         
         //enquanto o início for menor que o fim
         while(inicio <= fim){
-            
+            iteracoes++;
             meio = (inicio + fim)/2;
             
             //Verifica o meio sempre
             if(val == vetor[meio]){
+              System.out.println("Iteracoes: " + iteracoes);
                 return meio; 
             }else if(val > vetor[meio]){//direita
                 inicio = meio + 1;
             }else{//esquerda
                 fim = meio -1;
             }
+//            iteracoes++;
         }
-        
+        System.out.println("Iteracoes: " + iteracoes);
         return -1;//not found
     }
     
